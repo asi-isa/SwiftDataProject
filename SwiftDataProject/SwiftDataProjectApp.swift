a01aa4eb46571737cf6428e5f4194f8c5090cf8f
asi-isa <ali.inceoglu@protonmail.com>
2025-12-06 18:17:28 +0100

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Ali Soner Inceoglu on 06.12.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
